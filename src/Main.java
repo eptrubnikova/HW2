f257c75d9f726c4b7909fc00959571617b239fb6
eptrubnikova <eptrubnikova@gmail.com>
2022-09-21 22:44:54 +0600

    Вывод "Задача 5"

--- src/Main.java
+++ src/Main.java
@@ -28,5 +28,14 @@ public class Main {
         System.out.println(friend);
         friend = friend / 7;
         System.out.println(friend);
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
     }
 }
